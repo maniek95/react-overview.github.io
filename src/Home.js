612b2080ae2886b7fd9f5a4ae10dbf120cce371c
mmanka277411 <mmanka@student.agh.edu.pl>
2019-03-21 15:10:57 +0100

    adding some components just overview

--- src/Home.js
+++ src/Home.js
@@ -1,22 +1,24 @@
 import React, { Component } from 'react';
 import './Home.css';
-
+import HelloWithJSX from './WithJSX';
 class Home extends Component {
 showImageBeforeMount = false;
-componentWillMount() {
-    this.showImageBeforeMount = true;
-}
+
   render() {
     
     return (
       <div className="App">
         <header className="App-header">
+        <img src="./img/heart.png" />
+
           <p>
             Home Test
           </p>
-        
-        {this.showImageBeforeMount && '<p>1</p>'}
-    
+        This is how to React works!
+        <HelloWithJSX toWhat="World" />
+        <HelloWithJSX toWhat="People" />
+        <HelloWithJSX toWhat="Everyone!" />
+
         </header>
       </div>
     );
--- src/LifecycleComponent.js
+++ src/LifecycleComponent.js
@@ -1,22 +1,48 @@
 import React, { Component } from 'react';
 import './LifecycleComponent.css';
+import Clock from './Clock';
+
 
 class LifecycleComponent extends Component {
-showImageBeforeMount = false;
-componentWillMount() {
-    this.showImageBeforeMount = true;
-}
+  // updateSession = false;
+  // demount = false;
+  // mountSession = false;
+// componentWillMount() {
+//   this.updateSession = true;
+// }
+// componentDidMount() {
+//   this.mountSession = true;
+// }
+
   render() {
     
     return (
       <div className="App">
         <header className="App-header">
-          <p>
+          <h1>
             Lifecycle Test
-          </p>
-        
-        {this.showImageBeforeMount && '<p>1</p>'}
-    
+          </h1>
+        To test lifecycle we used example from React documentation :) 
+
+        <Clock/>
+        {/* Mounted:
+        {this.mountSession && 
+        <div>
+        <h5>Component Did Mount</h5>
+        <img src="./img/heart.png" />
+        </div> }  
+        Update:
+        {this.updateSession && 
+        <div>
+        <h5>Component Will Mount</h5>
+        <img src="./img/heart.png" />
+        </div> }
+         Demount:
+         {this.demount && 
+        <div>
+        <h5>componentWillUnmount</h5>
+        <img src="./img/heart.png" />
+        </div> }   */}
         </header>
       </div>
     );
